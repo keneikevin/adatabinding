e11f0932d1b47d0b3d2366429564f49322b2a261
keneikevin <kevinkenei9@gmail.com>
2019-12-15 23:54:54 +0300

    creating binding adapters

--- app/src/main/java/com/example/android/databinding/basicsample/ui/PlainOldActivity.kt
+++ app/src/main/java/com/example/android/databinding/basicsample/ui/PlainOldActivity.kt
@@ -32,7 +32,9 @@ import androidx.lifecycle.ViewModelProviders
 import com.example.android.databinding.basicsample.R
 import com.example.android.databinding.basicsample.data.Popularity
 import com.example.android.databinding.basicsample.data.SimpleViewModel
+import com.example.android.databinding.basicsample.data.SimpleViewModelSolution
 import com.example.android.databinding.basicsample.databinding.PlainActivityBinding
+import com.example.android.databinding.basicsample.databinding.SolutionBinding
 
 /**
  * Plain old activity with lots of problems to fix.
@@ -40,75 +42,18 @@ import com.example.android.databinding.basicsample.databinding.PlainActivityBind
 class PlainOldActivity : AppCompatActivity() {
 
     // Obtain ViewModel from ViewModelProviders
-    private val viewModel by lazy { ViewModelProviders.of(this).get(SimpleViewModel::class.java) }
+    private val viewModel by lazy {
+        ViewModelProviders.of(this).get(SimpleViewModelSolution::class.java)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        val binding : PlainActivityBinding = DataBindingUtil.setContentView(this, R.layout.plain_activity)
+        val binding: SolutionBinding =
+                DataBindingUtil.setContentView(this, R.layout.solution)
 
-        // TODO: Explicitly setting initial values is a bad pattern. We'll fix that.
-        binding.name="kevin"
-        binding.lastname="kenei"
-    }
+        binding.lifecycleOwner = this  // use Fragment.viewLifecycleOwner for fragments
 
-    /**
-     * This method is triggered by the `android:onclick` attribute in the layout. It puts business
-     * logic in the activity, which is not ideal. We should do something about that.
-     */
-    fun onLike(view: View) {
-        viewModel.onLike()
-        updateLikes()
-    }
-
-    /**
-     * So much findViewById! We'll fix that with Data Binding.
-     */
-    private fun updateName() {
-        findViewById<TextView>(R.id.plain_name).text = viewModel.name
-        findViewById<TextView>(R.id.plain_lastname).text = viewModel.lastName
-    }
-
-    /**
-     * This method has many problems:
-     * - It's calling findViewById multiple times
-     * - It has untestable logic
-     * - It's updating two views when called even if they're not changing
-     */
-    private fun updateLikes() {
-        findViewById<TextView>(R.id.likes).text = viewModel.likes.toString()
-        findViewById<ProgressBar>(R.id.progressBar).progress =
-            (viewModel.likes * 100 / 5).coerceAtMost(100)
-        val image = findViewById<ImageView>(R.id.imageView)
-
-        val color = getAssociatedColor(viewModel.popularity, this)
-
-        ImageViewCompat.setImageTintList(image, ColorStateList.valueOf(color))
-
-        image.setImageDrawable(getDrawablePopularity(viewModel.popularity, this))
-    }
-
-    private fun getAssociatedColor(popularity: Popularity, context: Context): Int {
-        return when (popularity) {
-            Popularity.NORMAL -> context.theme.obtainStyledAttributes(
-                intArrayOf(android.R.attr.colorForeground)
-            ).getColor(0, 0x000000)
-            Popularity.POPULAR -> ContextCompat.getColor(context, R.color.popular)
-            Popularity.STAR -> ContextCompat.getColor(context, R.color.star)
-        }
-    }
-
-    private fun getDrawablePopularity(popularity: Popularity, context: Context): Drawable? {
-        return when (popularity) {
-            Popularity.NORMAL -> {
-                ContextCompat.getDrawable(context, R.drawable.ic_person_black_96dp)
-            }
-            Popularity.POPULAR -> {
-                ContextCompat.getDrawable(context, R.drawable.ic_whatshot_black_96dp)
-            }
-            Popularity.STAR -> {
-                ContextCompat.getDrawable(context, R.drawable.ic_whatshot_black_96dp)
-            }
-        }
+        binding.viewmodel = viewModel
     }
 }
